00370bc8ba6ba01f20a5db0ff29a530d98eb5f4b
rainyjune <rainyjune@live.cn>
2015-11-09 10:45:53 +0800

    Add test case for the element property

--- test/tests.js
+++ test/tests.js
@@ -1,36 +1,48 @@
 $(function(){
+  var obj = new Css3Rotator("#gallery", {
+    container: '.list',
+    autoPlay: false,
+    dataSource: [
+      {
+        title: 'The first photo',
+        img: '../demo/images/abf6afa0bb6de81d7abb16923b3_p1_mk1.jpg',
+        link: 'http://m.leju.com/touch/news/c/bj/5984223020636746567.html'
+      },
+      {
+        title: '不愁房租涨首付两万五买房靠谱',
+        img: '../demo/images/d41c8d152cc8554132424b96cd5_p1_mk1.jpg',
+        link: 'http://m.leju.com/touch/news/c/bj/5986396554918146726.html'
+      }
+    ]
+  });
+
   QUnit.module("Load test");
   QUnit.test( "Object test", function( assert ) {
     assert.expect(2);
-    var obj = $("#gallery").css3Rotator({
-      container: '.list',
-      autoPlay: false,
-      dataSource: [
-        {
-          title: 'The first photo',
-          img: '../demo/images/abf6afa0bb6de81d7abb16923b3_p1_mk1.jpg',
-          link: 'http://m.leju.com/touch/news/c/bj/5984223020636746567.html'
-        },
-        {
-          title: '不愁房租涨首付两万五买房靠谱',
-          img: '../demo/images/d41c8d152cc8554132424b96cd5_p1_mk1.jpg',
-          link: 'http://m.leju.com/touch/news/c/bj/5986396554918146726.html'
-        }
-      ]
-    });
 
     assert.ok(typeof obj !== "undefined", "The obj variable is defined.");
     alert("Please swipe left and make sure the second was changed as the active one");
     var done = assert.async();
-    obj.on("slidechanged", function(event, currentIndex){
+    obj.addEventListener("slidechanged", slidechangeHandler);
+
+    function slidechangeHandler(event, currentIndex) {
       assert.strictEqual(currentIndex, 2, "The slide changed to the second one");
       done();
-      obj.off("slidechanged");
-    });
+      obj.removeEventListener("slidechanged", slidechangeHandler);
+    }
 
   });
 
-  QUnit.test("The next test", function(assert) {
+  QUnit.module("Properties Test");
+  QUnit.test("The element property", function(assert) {
+    assert.equal(obj.element.attr("id"), "gallery", "The id of root element is #gallery");
+    obj.element = $("body");
+    assert.equal(obj.element.attr("id"), "gallery", "The root element is still #gallery");
+  });
+
+  QUnit.module("Dispose test");
+
+  QUnit.test("Dispose the obj object", function(assert) {
     assert.ok(true, "Always true");
   });
   
